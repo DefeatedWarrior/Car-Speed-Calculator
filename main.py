23105a5d5290b5ffd791eb3f37c64434deedbac6
DefeatedWarrior <55876116+DefeatedWarrior@users.noreply.github.com>
2024-03-07 10:32:50 +1100

    Add files via upload
    update to working script

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
+nr = 0
+dist = 30
 
 with open('data.csv', 'w') as file:
-    file.write("#,Time,Speed,Distance,Type")
+    file.write("#,Time,Speed (KM/H),Speed (M/S),Distance,Type")
     file.close()
 
-def Ask_Questions():
+def Ask_Questions(nr):
     times = input("Time: ")
+    if times == "exit":
+        file.close()
+        exit()
+    else:
+        times = int(times)
+
     car_type = input("Car Type: ")
-    CSV_Add_Line(times, car_type)
+    nr = nr + 1
+    CSV_Add_Line(times, car_type, nr, dist)
 
-def CSV_Add_Line(t, name):
+def CSV_Add_Line(t, name, nr, dist):
+    s = dist/t
+    km = str(s * 3.6)
     with open('data.csv', 'a') as file:
-        file.write()
+        file.write("\n"+ str(nr) + "," + str(t) + "," + km + "," + str(s) + "," + str(dist) + "," + name + ",")
+        Ask_Questions(nr)
 
-Ask_Questions()
+Ask_Questions(nr)
